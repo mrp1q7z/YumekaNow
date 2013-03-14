fdd12ccb35ce4a42057707854d98674855a30f2d
taoka <mrp1q7z@gmail.com>
2013-03-14 21:43:07 +0900

    カードフラグメントの見た目をクールに

--- src/com/yojiokisoft/yumekanow/fragment/CardFragment.java
+++ src/com/yojiokisoft/yumekanow/fragment/CardFragment.java
@@ -1,11 +1,13 @@
 package com.yojiokisoft.yumekanow.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 import com.yojiokisoft.yumekanow.R;
@@ -24,10 +26,16 @@ public class CardFragment extends Fragment {
 		view = inflater.inflate(R.layout.fragment_card, container, false);
 
 		LinearLayout back = (LinearLayout) view.findViewById(R.id.affirmationBack);
-		back.setBackgroundResource(R.drawable.image_8);
-//		back.setGravity(Gravity.CENTER_VERTICAL);
+		back.setBackgroundResource(R.drawable.back_img01);
+		//		back.setGravity(Gravity.CENTER_VERTICAL);
 
 		TextView text = (TextView) view.findViewById(R.id.affirmationText);
+		text.setTextColor(0xff333333);
+		text.setTextSize(20.0f);
+		LayoutParams params = (LayoutParams) text.getLayoutParams();
+		params.setMargins(30, 120, 0, 0);
+		text.setLayoutParams(params);
+		text.setShadowLayer(1.5f, 1.0f, 1.0f, Color.WHITE);
 		text.setText("すごく調子がいい\n頭も冴えて\nエネルギーで満ち溢れています");
 
 		return view;
