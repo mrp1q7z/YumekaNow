63b7d1cea5716a5e5c83b348235ad9c50ce73860
taoka <mrp1q7z@gmail.com>
2013-04-13 16:09:21 +0900

    カード詳細画面でページャーでカードの切り替えができるようにした

--- src/com/yojiokisoft/yumekanow/activity/CardDetailActivity.java
+++ src/com/yojiokisoft/yumekanow/activity/CardDetailActivity.java
@@ -1,6 +1,7 @@
 package com.yojiokisoft.yumekanow.activity;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
@@ -9,16 +10,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
-import android.view.MotionEvent;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.j256.ormlite.dao.Dao;
 import com.yojiokisoft.yumekanow.R;
@@ -26,17 +29,8 @@ import com.yojiokisoft.yumekanow.db.DatabaseHelper;
 import com.yojiokisoft.yumekanow.entity.CardEntity;
 
 public class CardDetailActivity extends Activity {
-	private static final int SWIPE_MIN_DISTANCE = 120;
-	private static final int SWIPE_MAX_OFF_PATH = 250;
-	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
-
-	private GestureDetector mGestureDetector;
-	private TextView mTextView;
-	private ImageView mImageView;
-	private LinearLayout mTextContainer;
-	private int mPosition;
-	private List<CardEntity> mList;
 	private Context mContext;
+	private ViewPager mViewPager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,84 +44,45 @@ public class CardDetailActivity extends Activity {
 			return;
 		}
 		CardEntity card = (CardEntity) extras.getSerializable("Card");
-		mPosition = extras.getInt("Position");
-
-		mTextView = (TextView) findViewById(R.id.affirmationText);
-		mTextContainer = (LinearLayout) findViewById(R.id.textContainer);
-		mImageView = (ImageView) findViewById(R.id.backImage);
-		
-		printCard(card);
 
 		Button useButton = (Button) findViewById(R.id.useButton);
 		Button editButton = (Button) findViewById(R.id.editButton);
 
-		mGestureDetector = new GestureDetector(this, mOnGestureListener);
 		DatabaseHelper helper = new DatabaseHelper(this);
 		Dao<CardEntity, Integer> cardDao;
+		List<CardEntity> list = null;
 		try {
 			cardDao = helper.getDao(CardEntity.class);
-			mList = cardDao.queryForAll();
+			list = cardDao.queryForAll();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		useButton.setOnClickListener(mUseButtonOnClick);
 		editButton.setOnClickListener(mEditButtonOnClick);
-	}
-	
-	private void printCard(CardEntity card) {
-		mTextView.setText(card.affirmationText);
-		mTextView.setTextColor(card.textColor);
-		mTextView.setShadowLayer(1.5f, 1.5f, 1.5f, card.shadowColor);
-		mTextView.setTextSize(card.textSize);
-		mTextContainer.setPadding(card.marginLeft, card.marginTop, 0, 0);
 
-		mImageView.setImageResource(card.backImageResourceId);
-	}
-
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		mGestureDetector.onTouchEvent(event);
-		return super.onTouchEvent(event);
-	}
-
-	private final SimpleOnGestureListener mOnGestureListener = new SimpleOnGestureListener() {
-
-		@Override
-		public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
-			if (Math.abs(event1.getX() - event2.getX()) > SWIPE_MAX_OFF_PATH) {
-				return false;
+		CustomPagerAdapter adapter = new CustomPagerAdapter(this);
+		CardEntity cardEntity;
+		int page = 0;
+		for (int i = 0; i < list.size(); i++) {
+			cardEntity = list.get(i);
+			adapter.add(cardEntity.id);
+			if (cardEntity.id == card.id) {
+				page = i;
 			}
-			if (event1.getX() - event2.getX() > SWIPE_MIN_DISTANCE
-					&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-				if (mPosition < (mList.size() - 1)) {
-					mPosition++;
-					CardEntity card = mList.get(mPosition);
-					printCard(card);
-				} else {
-					Toast.makeText(CardDetailActivity.this, "次ページはありません", Toast.LENGTH_SHORT).show();
-					return false;
-				}
-			} else if (event2.getX() - event1.getX() > SWIPE_MIN_DISTANCE
-					&& Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-				if (mPosition > 0) {
-					mPosition--;
-					CardEntity card = mList.get(mPosition);
-					printCard(card);
-				} else {
-					Toast.makeText(CardDetailActivity.this, "前ページはありません", Toast.LENGTH_SHORT).show();
-					return false;
-				}
-			}
-			return false;
 		}
-	};
+
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(adapter);
+		mViewPager.setCurrentItem(page);
+	}
 
 	// このカードを使うボタンクリック
 	private final OnClickListener mUseButtonOnClick = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			CardEntity card = mList.get(mPosition);
+			CustomPagerAdapter adapter = (CustomPagerAdapter) mViewPager.getAdapter();
+			CardEntity card = adapter.getCard(mViewPager.getCurrentItem());
 			SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(mContext);
 			sp.edit().putInt("UseCard", card.id).commit();
 			finish();
@@ -138,11 +93,117 @@ public class CardDetailActivity extends Activity {
 	private final OnClickListener mEditButtonOnClick = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			CardEntity card = mList.get(mPosition);
+			CustomPagerAdapter adapter = (CustomPagerAdapter) mViewPager.getAdapter();
+			CardEntity card = adapter.getCard(mViewPager.getCurrentItem());
 			Intent myIntent = new Intent(getApplicationContext(), MakeCardActivity.class);
 			myIntent.putExtra("Card", card);
 			myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			startActivity(myIntent);
 		}
 	};
+
+	public class CustomPagerAdapter extends PagerAdapter {
+
+		private TextView mTextView;
+		private ImageView mImageView;
+		private LinearLayout mTextContainer;
+		private Dao<CardEntity, Integer> mCardDao;
+		/** コンテキスト. */
+		private Context mContext;
+
+		/** リスト. */
+		private ArrayList<Integer> mList;
+
+		private LayoutInflater mInflter;
+
+		/**
+		 * コンストラクタ.
+		 */
+		public CustomPagerAdapter(Context context) {
+			mContext = context;
+			mInflter = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			mList = new ArrayList<Integer>();
+
+			DatabaseHelper helper = new DatabaseHelper(mContext);
+			try {
+				mCardDao = helper.getDao(CardEntity.class);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		/**
+		 * リストにアイテムを追加する.
+		 * @param item アイテム
+		 */
+		public void add(Integer item) {
+			mList.add(item);
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+			FrameLayout layout = (FrameLayout) this.mInflter.inflate(R.layout.card_detail, null);
+
+			mTextView = (TextView) layout.findViewById(R.id.affirmationText);
+			mTextContainer = (LinearLayout) layout.findViewById(R.id.textContainer);
+			mImageView = (ImageView) layout.findViewById(R.id.backImage);
+
+			// リストから取得
+			Integer cardId = mList.get(position);
+			Log.d("taoka", "pos=" + position + ",cardId=" + cardId);
+			try {
+				List<CardEntity> list = mCardDao.queryForEq("id", cardId);
+				printCard(list.get(0));
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			// コンテナに追加
+			container.addView(layout);
+
+			return layout;
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) {
+			// コンテナから View を削除
+			container.removeView((View) object);
+		}
+
+		@Override
+		public int getCount() {
+			// リストのアイテム数を返す
+			return mList.size();
+		}
+
+		@Override
+		public boolean isViewFromObject(View view, Object object) {
+			// Object 内に View が存在するか判定する
+			return view == (FrameLayout) object;
+		}
+
+		private void printCard(CardEntity card) {
+			mTextView.setText(card.affirmationText);
+			mTextView.setTextColor(card.textColor);
+			mTextView.setShadowLayer(1.5f, 1.5f, 1.5f, card.shadowColor);
+			mTextView.setTextSize(card.textSize);
+			mTextContainer.setPadding(card.marginLeft, card.marginTop, 0, 0);
+
+			mImageView.setImageResource(card.backImageResourceId);
+		}
+
+		public CardEntity getCard(int position) {
+			int cardId = mList.get(position);
+			try {
+				List<CardEntity> list = mCardDao.queryForEq("id", cardId);
+				return list.get(0);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
 }
