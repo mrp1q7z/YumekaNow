ebb6136a9cbd64ecc73b6e7d7a1c0b9593458ee5
taoka <mrp1q7z@gmail.com>
2013-07-07 08:45:08 +0900

    Javadocの記入(entity)

--- src/com/yojiokisoft/yumekanow/entity/CardEntity.java
+++ src/com/yojiokisoft/yumekanow/entity/CardEntity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2013 YojiokiSoft
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.yojiokisoft.yumekanow.entity;
 
 import java.io.Serializable;
@@ -5,24 +20,44 @@ import java.io.Serializable;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
-@DatabaseTable(tableName="card")
+/**
+ * カード情報エンティティ
+ */
+@DatabaseTable(tableName = "card")
 public class CardEntity implements Serializable {
+	/** カードID */
 	@DatabaseField(generatedId = true)
-	public int id; // カードID
+	public int id;
+
+	/** 背景画像のリソースID */
 	@DatabaseField
-	public int backImageResourceId; // 背景画像のリソースID
+	public int backImageResourceId;
+
+	/** 背景画像のパス */
 	@DatabaseField
-	public String backImagePath; // 背景画像のパス
+	public String backImagePath;
+
+	/** アファーメーションの言葉 */
 	@DatabaseField
-	public String affirmationText; // アファーメーションの言葉
+	public String affirmationText;
+
+	/** 文字の色 */
 	@DatabaseField
-	public int textColor; // 文字の色
+	public int textColor;
+
+	/** 影の色 */
 	@DatabaseField
-	public int shadowColor; // 影の色
+	public int shadowColor;
+
+	/** 文字の大きさ */
 	@DatabaseField
-	public int textSize; // 文字の大きさ
+	public int textSize;
+
+	/** 上マージン */
 	@DatabaseField
-	public int marginTop; // 上マージン
+	public int marginTop;
+
+	/** 左マージン */
 	@DatabaseField
-	public int marginLeft; // 左マージン
+	public int marginLeft;
 }
--- src/com/yojiokisoft/yumekanow/entity/CounterEntity.java
+++ src/com/yojiokisoft/yumekanow/entity/CounterEntity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2013 YojiokiSoft
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.yojiokisoft.yumekanow.entity;
 
 import java.io.Serializable;
@@ -5,16 +20,28 @@ import java.io.Serializable;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
+/**
+ * 唱えた回数エンティティ
+ */
 @DatabaseTable(tableName = "counter")
 public class CounterEntity implements Serializable {
+	/** カードID */
 	@DatabaseField(uniqueCombo = true)
-	public int cardId; // カードID
+	public int cardId;
+
+	/** 処理時間（積算ミリ秒） */
 	@DatabaseField(uniqueCombo = true)
-	public long procTime; // 処理時間（積算ミリ秒）
+	public long procTime;
+
+	/** OKカウンタ（OKボタンを押して閉じた時＋１） */
 	@DatabaseField
-	public int okCnt; // OKカウンタ
+	public int okCnt;
+
+	/** NGカウンタ（キャンセルボタンを押して閉じたとき＋１） */
 	@DatabaseField
-	public int ngCnt; // NGカウンタ
+	public int ngCnt;
+
+	/** 処理日(yyyymmdd) */
 	@DatabaseField
-	public String procDay; // 処理日(yyyymmdd)
+	public String procDay;
 }
