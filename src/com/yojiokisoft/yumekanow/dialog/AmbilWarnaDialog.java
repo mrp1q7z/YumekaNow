46539b39937243405420a070b660e9603ec004ed
taoka <mrp1q7z@gmail.com>
2013-07-06 21:46:51 +0900

    Javadocの記入(dialog)

--- src/com/yojiokisoft/yumekanow/dialog/AmbilWarnaDialog.java
+++ src/com/yojiokisoft/yumekanow/dialog/AmbilWarnaDialog.java
@@ -29,192 +29,210 @@ import android.widget.RelativeLayout;
 import com.yojiokisoft.yumekanow.R;
 
 public class AmbilWarnaDialog {
-        public interface OnAmbilWarnaListener {
-                void onCancel(AmbilWarnaDialog dialog);
-                void onOk(AmbilWarnaDialog dialog, int color);
-        }
+	public interface OnAmbilWarnaListener {
+		void onCancel(AmbilWarnaDialog dialog);
 
-        final AlertDialog dialog;
-        final OnAmbilWarnaListener listener;
-        final View viewHue;
-        final AmbilWarnaKotak viewSatVal;
-        final ImageView viewCursor;
-        final View viewOldColor;
-        final View viewNewColor;
-        final ImageView viewTarget;
-        final ViewGroup viewContainer;
-        final float[] currentColorHsv = new float[3];
+		void onOk(AmbilWarnaDialog dialog, int color);
+	}
 
-        /**
-         * create an AmbilWarnaDialog. call this only from OnCreateDialog() or from a background thread.
-         *
-         * @param context
-         *            current context
-         * @param color
-         *            current color
-         * @param listener
-         *            an OnAmbilWarnaListener, allowing you to get back error or
-         */
-        public AmbilWarnaDialog(final Context context, int color, OnAmbilWarnaListener listener) {
-                this.listener = listener;
-                Color.colorToHSV(color, currentColorHsv);
+	final AlertDialog dialog;
+	final OnAmbilWarnaListener listener;
+	final View viewHue;
+	final AmbilWarnaKotak viewSatVal;
+	final ImageView viewCursor;
+	final View viewOldColor;
+	final View viewNewColor;
+	final ImageView viewTarget;
+	final ViewGroup viewContainer;
+	final float[] currentColorHsv = new float[3];
 
-                final View view = LayoutInflater.from(context).inflate(R.layout.ambilwarna_dialog, null);
-                viewHue = view.findViewById(R.id.ambilwarna_viewHue);
-                viewSatVal = (AmbilWarnaKotak) view.findViewById(R.id.ambilwarna_viewSatBri);
-                viewCursor = (ImageView) view.findViewById(R.id.ambilwarna_cursor);
-                viewOldColor = view.findViewById(R.id.ambilwarna_warnaLama);
-                viewNewColor = view.findViewById(R.id.ambilwarna_warnaBaru);
-                viewTarget = (ImageView) view.findViewById(R.id.ambilwarna_target);
-                viewContainer = (ViewGroup) view.findViewById(R.id.ambilwarna_viewContainer);
+	/**
+	 * create an AmbilWarnaDialog. call this only from OnCreateDialog() or from a background thread.
+	 *
+	 * @param context
+	 *            current context
+	 * @param color
+	 *            current color
+	 * @param listener
+	 *            an OnAmbilWarnaListener, allowing you to get back error or
+	 */
+	public AmbilWarnaDialog(final Context context, int color, OnAmbilWarnaListener listener) {
+		this.listener = listener;
+		Color.colorToHSV(color, currentColorHsv);
 
-                viewSatVal.setHue(getHue());
-                viewOldColor.setBackgroundColor(color);
-                viewNewColor.setBackgroundColor(color);
+		final View view = LayoutInflater.from(context).inflate(R.layout.ambilwarna_dialog, null);
+		viewHue = view.findViewById(R.id.ambilwarna_viewHue);
+		viewSatVal = (AmbilWarnaKotak) view.findViewById(R.id.ambilwarna_viewSatBri);
+		viewCursor = (ImageView) view.findViewById(R.id.ambilwarna_cursor);
+		viewOldColor = view.findViewById(R.id.ambilwarna_warnaLama);
+		viewNewColor = view.findViewById(R.id.ambilwarna_warnaBaru);
+		viewTarget = (ImageView) view.findViewById(R.id.ambilwarna_target);
+		viewContainer = (ViewGroup) view.findViewById(R.id.ambilwarna_viewContainer);
 
-                viewHue.setOnTouchListener(new View.OnTouchListener() {
-                        @Override public boolean onTouch(View v, MotionEvent event) {
-                                if (event.getAction() == MotionEvent.ACTION_MOVE
-                                                || event.getAction() == MotionEvent.ACTION_DOWN
-                                                || event.getAction() == MotionEvent.ACTION_UP) {
+		viewSatVal.setHue(getHue());
+		viewOldColor.setBackgroundColor(color);
+		viewNewColor.setBackgroundColor(color);
 
-                                        float y = event.getY();
-                                        if (y < 0.f) y = 0.f;
-                                        if (y > viewHue.getMeasuredHeight()) y = viewHue.getMeasuredHeight() - 0.001f; // to avoid looping from end to start.
-                                        float hue = 360.f - 360.f / viewHue.getMeasuredHeight() * y;
-                                        if (hue == 360.f) hue = 0.f;
-                                        setHue(hue);
+		viewHue.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_MOVE
+						|| event.getAction() == MotionEvent.ACTION_DOWN
+						|| event.getAction() == MotionEvent.ACTION_UP) {
 
-                                        // update view
-                                        viewSatVal.setHue(getHue());
-                                        moveCursor();
-                                        viewNewColor.setBackgroundColor(getColor());
+					float y = event.getY();
+					if (y < 0.f)
+						y = 0.f;
+					if (y > viewHue.getMeasuredHeight())
+						y = viewHue.getMeasuredHeight() - 0.001f; // to avoid looping from end to start.
+					float hue = 360.f - 360.f / viewHue.getMeasuredHeight() * y;
+					if (hue == 360.f)
+						hue = 0.f;
+					setHue(hue);
 
-                                        return true;
-                                }
-                                return false;
-                        }
-                });
-                viewSatVal.setOnTouchListener(new View.OnTouchListener() {
-                        @Override public boolean onTouch(View v, MotionEvent event) {
-                                if (event.getAction() == MotionEvent.ACTION_MOVE
-                                                || event.getAction() == MotionEvent.ACTION_DOWN
-                                                || event.getAction() == MotionEvent.ACTION_UP) {
+					// update view
+					viewSatVal.setHue(getHue());
+					moveCursor();
+					viewNewColor.setBackgroundColor(getColor());
 
-                                        float x = event.getX(); // touch event are in dp units.
-                                        float y = event.getY();
+					return true;
+				}
+				return false;
+			}
+		});
+		viewSatVal.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_MOVE
+						|| event.getAction() == MotionEvent.ACTION_DOWN
+						|| event.getAction() == MotionEvent.ACTION_UP) {
 
-                                        if (x < 0.f) x = 0.f;
-                                        if (x > viewSatVal.getMeasuredWidth()) x = viewSatVal.getMeasuredWidth();
-                                        if (y < 0.f) y = 0.f;
-                                        if (y > viewSatVal.getMeasuredHeight()) y = viewSatVal.getMeasuredHeight();
+					float x = event.getX(); // touch event are in dp units.
+					float y = event.getY();
 
-                                        setSat(1.f / viewSatVal.getMeasuredWidth() * x);
-                                        setVal(1.f - (1.f / viewSatVal.getMeasuredHeight() * y));
+					if (x < 0.f)
+						x = 0.f;
+					if (x > viewSatVal.getMeasuredWidth())
+						x = viewSatVal.getMeasuredWidth();
+					if (y < 0.f)
+						y = 0.f;
+					if (y > viewSatVal.getMeasuredHeight())
+						y = viewSatVal.getMeasuredHeight();
 
-                                        // update view
-                                        moveTarget();
-                                        viewNewColor.setBackgroundColor(getColor());
+					setSat(1.f / viewSatVal.getMeasuredWidth() * x);
+					setVal(1.f - (1.f / viewSatVal.getMeasuredHeight() * y));
 
-                                        return true;
-                                }
-                                return false;
-                        }
-                });
+					// update view
+					moveTarget();
+					viewNewColor.setBackgroundColor(getColor());
 
-                dialog = new AlertDialog.Builder(context)
-                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                                @Override public void onClick(DialogInterface dialog, int which) {
-                                        if (AmbilWarnaDialog.this.listener != null) {
-                                                AmbilWarnaDialog.this.listener.onOk(AmbilWarnaDialog.this, getColor());
-                                        }
-                                }
-                        })
-                        .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                                @Override public void onClick(DialogInterface dialog, int which) {
-                                        if (AmbilWarnaDialog.this.listener != null) {
-                                                AmbilWarnaDialog.this.listener.onCancel(AmbilWarnaDialog.this);
-                                        }
-                                }
-                        })
-                        .setOnCancelListener(new OnCancelListener() {
-                                // if back button is used, call back our listener.
-                                @Override public void onCancel(DialogInterface paramDialogInterface) {
-                                        if (AmbilWarnaDialog.this.listener != null) {
-                                                AmbilWarnaDialog.this.listener.onCancel(AmbilWarnaDialog.this);
-                                        }
+					return true;
+				}
+				return false;
+			}
+		});
 
-                                }
-                        })
-                        .create();
-                // kill all padding from the dialog window
-                dialog.setView(view, 0, 0, 0, 0);
+		dialog = new AlertDialog.Builder(context)
+				.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						if (AmbilWarnaDialog.this.listener != null) {
+							AmbilWarnaDialog.this.listener.onOk(AmbilWarnaDialog.this, getColor());
+						}
+					}
+				})
+				.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						if (AmbilWarnaDialog.this.listener != null) {
+							AmbilWarnaDialog.this.listener.onCancel(AmbilWarnaDialog.this);
+						}
+					}
+				})
+				.setOnCancelListener(new OnCancelListener() {
+					// if back button is used, call back our listener.
+					@Override
+					public void onCancel(DialogInterface paramDialogInterface) {
+						if (AmbilWarnaDialog.this.listener != null) {
+							AmbilWarnaDialog.this.listener.onCancel(AmbilWarnaDialog.this);
+						}
 
-                // move cursor & target on first draw
-                ViewTreeObserver vto = view.getViewTreeObserver();
-                vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-                        @Override public void onGlobalLayout() {
-                                moveCursor();
-                                moveTarget();
-                                view.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                        }
-                });
-        }
+					}
+				})
+				.create();
+		// kill all padding from the dialog window
+		dialog.setView(view, 0, 0, 0, 0);
 
-        protected void moveCursor() {
-                float y = viewHue.getMeasuredHeight() - (getHue() * viewHue.getMeasuredHeight() / 360.f);
-                if (y == viewHue.getMeasuredHeight()) y = 0.f;
-                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) viewCursor.getLayoutParams();
-                layoutParams.leftMargin = (int) (viewHue.getLeft() - Math.floor(viewCursor.getMeasuredWidth() / 2) - viewContainer.getPaddingLeft());
-                ;
-                layoutParams.topMargin = (int) (viewHue.getTop() + y - Math.floor(viewCursor.getMeasuredHeight() / 2) - viewContainer.getPaddingTop());
-                ;
-                viewCursor.setLayoutParams(layoutParams);
-        }
+		// move cursor & target on first draw
+		ViewTreeObserver vto = view.getViewTreeObserver();
+		vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+			@Override
+			public void onGlobalLayout() {
+				moveCursor();
+				moveTarget();
+				view.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+			}
+		});
+	}
 
-        protected void moveTarget() {
-                float x = getSat() * viewSatVal.getMeasuredWidth();
-                float y = (1.f - getVal()) * viewSatVal.getMeasuredHeight();
-                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) viewTarget.getLayoutParams();
-                layoutParams.leftMargin = (int) (viewSatVal.getLeft() + x - Math.floor(viewTarget.getMeasuredWidth() / 2) - viewContainer.getPaddingLeft());
-                layoutParams.topMargin = (int) (viewSatVal.getTop() + y - Math.floor(viewTarget.getMeasuredHeight() / 2) - viewContainer.getPaddingTop());
-                viewTarget.setLayoutParams(layoutParams);
-        }
+	protected void moveCursor() {
+		float y = viewHue.getMeasuredHeight() - (getHue() * viewHue.getMeasuredHeight() / 360.f);
+		if (y == viewHue.getMeasuredHeight())
+			y = 0.f;
+		RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) viewCursor.getLayoutParams();
+		layoutParams.leftMargin = (int) (viewHue.getLeft() - Math.floor(viewCursor.getMeasuredWidth() / 2) - viewContainer
+				.getPaddingLeft());
+		;
+		layoutParams.topMargin = (int) (viewHue.getTop() + y - Math.floor(viewCursor.getMeasuredHeight() / 2) - viewContainer
+				.getPaddingTop());
+		;
+		viewCursor.setLayoutParams(layoutParams);
+	}
 
-        private int getColor() {
-                return Color.HSVToColor(currentColorHsv);
-        }
+	protected void moveTarget() {
+		float x = getSat() * viewSatVal.getMeasuredWidth();
+		float y = (1.f - getVal()) * viewSatVal.getMeasuredHeight();
+		RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) viewTarget.getLayoutParams();
+		layoutParams.leftMargin = (int) (viewSatVal.getLeft() + x - Math.floor(viewTarget.getMeasuredWidth() / 2) - viewContainer
+				.getPaddingLeft());
+		layoutParams.topMargin = (int) (viewSatVal.getTop() + y - Math.floor(viewTarget.getMeasuredHeight() / 2) - viewContainer
+				.getPaddingTop());
+		viewTarget.setLayoutParams(layoutParams);
+	}
 
-        private float getHue() {
-                return currentColorHsv[0];
-        }
+	private int getColor() {
+		return Color.HSVToColor(currentColorHsv);
+	}
 
-        private float getSat() {
-                return currentColorHsv[1];
-        }
+	private float getHue() {
+		return currentColorHsv[0];
+	}
 
-        private float getVal() {
-                return currentColorHsv[2];
-        }
+	private float getSat() {
+		return currentColorHsv[1];
+	}
 
-        private void setHue(float hue) {
-                currentColorHsv[0] = hue;
-        }
+	private float getVal() {
+		return currentColorHsv[2];
+	}
 
-        private void setSat(float sat) {
-                currentColorHsv[1] = sat;
-        }
+	private void setHue(float hue) {
+		currentColorHsv[0] = hue;
+	}
 
-        private void setVal(float val) {
-                currentColorHsv[2] = val;
-        }
+	private void setSat(float sat) {
+		currentColorHsv[1] = sat;
+	}
 
-        public void show() {
-                dialog.show();
-        }
+	private void setVal(float val) {
+		currentColorHsv[2] = val;
+	}
 
-        public AlertDialog getDialog() {
-                return dialog;
-        }
+	public void show() {
+		dialog.show();
+	}
+
+	public AlertDialog getDialog() {
+		return dialog;
+	}
 }
-
--- src/com/yojiokisoft/yumekanow/dialog/VersionDialogPreference.java
+++ src/com/yojiokisoft/yumekanow/dialog/VersionDialogPreference.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2013 YojiokiSoft
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.yojiokisoft.yumekanow.dialog;
 
 import android.content.Context;
@@ -8,22 +23,26 @@ import android.widget.TextView;
 
 import com.yojiokisoft.yumekanow.R;
 
+/**
+ * 設定画面のバージョンダイアログ
+ */
 public class VersionDialogPreference extends DialogPreference {
-	private Context mContext;
-
+	/**
+	 * コンストラクタ.
+	 * 
+	 * @param context
+	 * @param attrs
+	 */
 	public VersionDialogPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		mContext = context;
 	}
 
-	public VersionDialogPreference(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		mContext = context;
-	}
-
+	/**
+	 * カスタムコンテンツビューの作成.
+	 */
 	@Override
 	protected View onCreateDialogView() {
-		String version = mContext.getResources().getString(R.string.version);
+		String version = this.getContext().getResources().getString(R.string.version);
 
 		TextView textView = new TextView(this.getContext());
 		textView.setText(version);
