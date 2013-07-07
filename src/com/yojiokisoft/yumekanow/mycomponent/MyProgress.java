0532424f2483ca4bb0f4f2b71e13312c8b2b0cd3
taoka <mrp1q7z@gmail.com>
2013-07-07 21:49:49 +0900

    Javadocの記入(mycomponent)

--- src/com/yojiokisoft/yumekanow/mycomponent/MyProgress.java
+++ src/com/yojiokisoft/yumekanow/mycomponent/MyProgress.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2013 YojiokiSoft
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.yojiokisoft.yumekanow.mycomponent;
 
 import android.content.Context;
@@ -11,7 +26,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.yojiokisoft.yumekanow.R;
+import com.yojiokisoft.yumekanow.utils.MyLog;
 
+/**
+ * カスタムプログレスバー
+ */
 public class MyProgress extends LinearLayout {
 
 	// パーセントテキスト
@@ -27,6 +46,12 @@ public class MyProgress extends LinearLayout {
 	// 注釈テキスト
 	private TextView mDescriptionText = null;
 
+	/**
+	 * コンストラクタ.
+	 * 
+	 * @param context
+	 * @param attrs
+	 */
 	public MyProgress(Context context, AttributeSet attrs) {
 		super(context, attrs);
 
@@ -85,6 +110,11 @@ public class MyProgress extends LinearLayout {
 		attrsArray.recycle();
 	}
 
+	/**
+	 * パーセント表示フラグのセット.
+	 * 
+	 * @param showFlag true=パーセントを表示する、false=しない
+	 */
 	public void setShowPercent(boolean showFlag) {
 		if (showFlag) {
 			mPercentText.setVisibility(VISIBLE);
@@ -93,6 +123,11 @@ public class MyProgress extends LinearLayout {
 		}
 	}
 
+	/**
+	 * 注釈に表示する文字をセット.
+	 * 
+	 * @param description
+	 */
 	public void setDescription(String description) {
 		if (description != null && description.length() > 0) {
 			mDescriptionText.setText(description);
@@ -102,9 +137,13 @@ public class MyProgress extends LinearLayout {
 		}
 	}
 
+	/**
+	 * メモリの開放
+	 */
 	public void drawableDestroy() {
+		MyLog.d("メモリを開放します");
 		Drawable drawable;
-		
+
 		drawable = mStartImageView.getDrawable();
 		if (drawable != null) {
 			drawable = null;
@@ -118,16 +157,29 @@ public class MyProgress extends LinearLayout {
 		}
 	}
 
+	/**
+	 * プログレスバーの上限値をセット.
+	 * 
+	 * @param max
+	 */
 	public void setMax(int max) {
 		mProgressBar.setMax(max);
 		setPercent();
 	}
 
+	/**
+	 * プログレスバーの進行具合をセット.
+	 * 
+	 * @param progress 0〜getMaxの範囲
+	 */
 	public void setProgress(int progress) {
 		mProgressBar.setProgress(progress);
 		setPercent();
 	}
 
+	/**
+	 * パーセントのセット
+	 */
 	private void setPercent() {
 		if (mProgressBar.getMax() != 0) {
 			int p = mProgressBar.getProgress() * 100 / mProgressBar.getMax();
